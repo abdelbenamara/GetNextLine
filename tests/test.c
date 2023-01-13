10007f0f1ffb08e24b5137e3f2b6db6df417b414
abdelbenamara <abdelb.contact@gmail.com>
2023-01-13 04:41:48 +0000

    Updated tests

--- tests/test.c
+++ tests/test.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 22:02:16 by abenamar          #+#    #+#             */
-/*   Updated: 2023/01/13 04:12:53 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/01/13 04:40:58 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,11 @@ int	main(void)
 {
 	printf("\nProject: " BOLDMAGENTA "Get Next Line\n");
 	printf(RESET "\n-\nPart 1: " BOLDMAGENTA "Mandatory\n");
-	printf(BOLDCYAN "\nFiles" RESET "\t\t" BOLDCYAN "Tests\n");
-	get_next_line_test("mandatory1.txt");
+	printf(BOLDCYAN "\nFile" RESET "\t\t\t\t" BOLDCYAN "Tests\n");
+	get_next_line_test("files/mandatory/simple.txt");
 #ifdef BONUS
 	printf("\n" RESET "\n-\nPart 2: " BOLDMAGENTA "Bonus" RESET "\n");
-	printf(BOLDCYAN "\nFiles" RESET "\t" BOLDCYAN "Tests" RESET "\n");
+	printf(BOLDCYAN "\nFile" RESET "\t\t\t\t" BOLDCYAN "Tests" RESET "\n");
 #endif
 	printf("\n" RESET "\n");
 	return (0);
