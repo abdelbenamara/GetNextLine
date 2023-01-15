9471a3bace914e36f0dd800796eb4395491afefc
abdelbenamara <abdelb.contact@gmail.com>
2023-01-15 04:47:43 +0000

    Updated get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -6,37 +6,50 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 01:35:25 by abenamar          #+#    #+#             */
-/*   Updated: 2023/01/13 03:49:10 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/01/15 01:12:48 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static size_t	get_line(char **line, char *buf)
+static int	init_str(char **str, size_t size)
 {
-	size_t	i;
-	size_t	len;
+	*str = malloc(size * sizeof(char));
+	if (!*str)
+		return (0);
+	**str = '\0';
+	return (1);
+}
+
+static int	update_line(char **line, size_t len, char *buf, size_t i)
+{
 	char	*tmp;
 
-	if (!(*line))
-	{
-		*line = malloc(1 * sizeof(char));
-		**line = '\0';
-		return (0);
-	}
-	i = 0;
-	while (buf[i] && buf[i] != '\n')
-		++i;
-	len = ft_strlen(*line);
-	tmp = malloc((len + i + 2) * sizeof(char));
+	tmp = malloc((len + i + 1) * sizeof(char));
 	if (!tmp)
 		return (0);
 	ft_strlcpy(tmp, *line, len + 1);
-	ft_strlcpy(tmp + len, buf, i + 2);
 	free(*line);
+	ft_strlcpy(tmp + len, buf, i + 1);
 	*line = tmp;
-	if (buf[i] == '\n' && ++i)
-		return (ft_strlcpy(buf, buf + i, BUFFER_SIZE), 1);
+	return (1);
+}
+
+static int	process_buffer(char *buf, char **line)
+{
+	size_t	i;
+
+	if (!*line)
+		return (!init_str(line, 1));
+	if (!*buf)
+		return (0);
+	i = 0;
+	while (buf[i] && buf[i] != '\n')
+		++i;
+	if (!update_line(line, ft_strlen(*line), buf, i + 1))
+		return (0);
+	if (buf[i] == '\n')
+		return (ft_strlcpy(buf, buf + i + 1, BUFFER_SIZE), 1);
 	return (ft_strlcpy(buf, buf + i, BUFFER_SIZE), 0);
 }
 
@@ -52,21 +65,18 @@ char	*get_next_line(int fd)
 	int			read_size;
 	char		*line;
 
-	if (!buf)
-	{
-		buf = malloc((BUFFER_SIZE + 1) * sizeof(char));
-		if (!buf)
-			return (NULL);
-		*buf = '\0';
-	}
+	if (fd < 0 || (!buf && !init_str(&buf, BUFFER_SIZE + 1)))
+		return (NULL);
 	line = NULL;
-	while (!get_line(&line, buf))
+	while (!process_buffer(buf, &line))
 	{
 		if (!*buf)
 		{
 			read_size = read(fd, buf, BUFFER_SIZE);
-			if (read_size <= 0)
-				return (free(line), reset_buffer(&buf), NULL);
+			if (*line && !read_size)
+				return (reset_buffer(&buf), line);
+			else if (read_size <= 0)
+				return (reset_buffer(&buf), free(line), NULL);
 			buf[read_size] = '\0';
 		}
 	}
