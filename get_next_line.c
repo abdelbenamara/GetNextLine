598e5aea72ff47e77b021ab99dfa0aa61ea29101
abdelbenamara <abdelb.contact@gmail.com>
2023-01-17 02:02:40 +0000

    Updated get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -6,81 +6,120 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 01:35:25 by abenamar          #+#    #+#             */
-/*   Updated: 2023/01/15 01:12:48 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/01/17 01:45:25 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static int	init_str(char **str, size_t size)
+static uint8_t	ft_buf_init(t_buf **buf, int fd)
 {
-	*str = malloc(size * sizeof(char));
-	if (!*str)
+	t_buf	*new;
+
+	new = malloc(sizeof(t_buf));
+	if (!new)
 		return (0);
-	**str = '\0';
+	new->fd = fd;
+	new->content = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!new->content)
+		return (free(new), 0);
+	*new->content = '\0';
+	new->next = NULL;
+	*buf = new;
 	return (1);
 }
 
-static int	update_line(char **line, size_t len, char *buf, size_t i)
+static uint8_t	ft_buf_poll(t_buf **lst, const int fd)
 {
+	t_buf	*buf;
+	t_buf	*prev;
+
+	buf = *lst;
+	prev = NULL;
+	while (buf)
+	{
+		if (buf->fd == fd)
+		{
+			if (prev)
+			{
+				prev->next = buf->next;
+				buf->next = *lst;
+			}
+			*lst = buf;
+			return (1);
+		}
+		prev = buf;
+		buf = prev->next;
+	}
+	if (!ft_buf_init(&buf, fd))
+		return (0);
+	buf->next = *lst;
+	*lst = buf;
+	return (1);
+}
+
+static uint8_t	ft_buf_to_line(t_buf *buf, char **line)
+{
+	size_t	i;
+	size_t	len;
 	char	*tmp;
 
-	tmp = malloc((len + i + 1) * sizeof(char));
+	if (!*buf->content)
+		return (0);
+	i = 0;
+	while (buf->content[i] && buf->content[i] != '\n')
+		++i;
+	len = ft_strlen(*line);
+	tmp = malloc((len + i + 2) * sizeof(char));
 	if (!tmp)
 		return (0);
 	ft_strlcpy(tmp, *line, len + 1);
 	free(*line);
-	ft_strlcpy(tmp + len, buf, i + 1);
+	ft_strlcpy(tmp + len, buf->content, i + 2);
 	*line = tmp;
-	return (1);
+	if (buf->content[i] == '\n')
+		return (ft_strlcpy(buf->content, buf->content + i + 1, BUFFER_SIZE), 1);
+	return (ft_strlcpy(buf->content, buf->content + i, BUFFER_SIZE), 0);
 }
 
-static int	process_buffer(char *buf, char **line)
+static void	ft_buf_pop(t_buf **lst)
 {
-	size_t	i;
+	t_buf	*buf;
 
-	if (!*line)
-		return (!init_str(line, 1));
-	if (!*buf)
-		return (0);
-	i = 0;
-	while (buf[i] && buf[i] != '\n')
-		++i;
-	if (!update_line(line, ft_strlen(*line), buf, i + 1))
-		return (0);
-	if (buf[i] == '\n')
-		return (ft_strlcpy(buf, buf + i + 1, BUFFER_SIZE), 1);
-	return (ft_strlcpy(buf, buf + i, BUFFER_SIZE), 0);
-}
-
-static void	reset_buffer(char **buf)
-{
-	free(*buf);
-	*buf = NULL;
+	if (*lst)
+	{
+		buf = *lst;
+		*lst = buf->next;
+		free(buf->content);
+		free(buf);
+	}
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*buf = NULL;
-	int			read_size;
-	char		*line;
+	static t_buf	*buf = NULL;
+	char			*line;
+	ssize_t			read_size;
 
-	if (fd < 0 || (!buf && !init_str(&buf, BUFFER_SIZE + 1)))
+	line = malloc(1 * sizeof(char));
+	if (!line)
 		return (NULL);
-	line = NULL;
-	while (!process_buffer(buf, &line))
+	if (fd < 0 || !ft_buf_poll(&buf, fd))
+		return (ft_buf_pop(&buf), free(line), NULL);
+	*line = '\0';
+	while (!ft_buf_to_line(buf, &line))
 	{
-		if (!*buf)
+		if (!*(buf->content))
 		{
-			read_size = read(fd, buf, BUFFER_SIZE);
+			read_size = read(fd, buf->content, BUFFER_SIZE);
 			if (*line && !read_size)
-				return (reset_buffer(&buf), line);
+				return (ft_buf_pop(&buf), line);
 			else if (read_size <= 0)
-				return (reset_buffer(&buf), free(line), NULL);
-			buf[read_size] = '\0';
+				return (ft_buf_pop(&buf), free(line), NULL);
+			buf->content[read_size] = '\0';
 		}
 	}
-	if (!*buf)
-		reset_buffer(&buf);
+	if (!*(buf->content))
+		return (ft_buf_pop(&buf), line);
 	return (line);
 }
