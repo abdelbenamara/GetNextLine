b78198f1663dfd9bc7d27f1cbd79e9d419e25508
abdelbenamara <abdelb.contact@gmail.com>
2023-01-17 19:45:07 +0000

    Updated tests

--- tests/gnl_invalid_test.c
+++ tests/gnl_invalid_test.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 23:39:55 by abenamar          #+#    #+#             */
-/*   Updated: 2023/01/15 21:59:14 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/01/17 19:44:21 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,18 @@
 
 void	gnl_invalid_test()
 {
-	const char	*filename = "files/invalid";
-	const int	fd = open(filename, O_RDONLY);
+	const char	*filename = "invalid";
+	int			fd;
 	char		*s;
 
 	printf(RESET "\n%s \t\t", filename);
+	fd = open("files/invalid_file_which_does_not_exist", O_RDONLY);
 	s = get_next_line(fd);
 	ft_assert(1, s == NULL);
-	s = get_next_line(-2);
+	fd = -256;
+	s = get_next_line(fd);
 	ft_assert(2, s == NULL);
+	fd = open("files/", O_RDONLY);
+	s = get_next_line(fd);
+	ft_assert(3, s == NULL);
 }
